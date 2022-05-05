83b529559d3628ac01c630a52b53ca6e1dd7cd37
Korrina-13 <94879154+Korrina-13@users.noreply.github.com>
2022-05-05 06:31:57 -0500

    update

--- project-b/five.js
+++ project-b/five.js
@@ -3,6 +3,6 @@ let randomWord;
 document.getElementById("myButton").onclick = function(){
     randomWord = document.getElementById("myText").value;
   console.log(randomWord);
-  document.getElementById("myLabelFive").innerHTML = randomWord + "heh-heh";
+  document.getElementById("myLabelFive").innerHTML = randomWord + " heh-heh";
   document.getElementById("commentFive").innerHTML = "I shall call you " + randomWord + " from now on";
 }
